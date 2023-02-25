180e63ec57443111fde26881ccd7be834eade50d
xBromsson <marvin@the-firebrand.com>
2023-02-25 02:23:22 -0600

    generated both x and y axis div grid

--- javascript.js
+++ javascript.js
@@ -1,13 +1,26 @@
 const xaxis = 16
 const yaxis = 16
 
-const containerx = document.querySelector('#xaxis.container');
+const body = document.querySelector('body');
 
-
-for (let i = 0; i < xaxis; i++) {
-
-    let cell = document.createElement('div')
-    cell.classList.add('cell')
-    containerx.appendChild(cell)
+for (let i = 0; i < yaxis; i++) {
+    let ycell = document.createElement('div');
+    ycell.classList.add('container');
+    ycell.setAttribute('id','yaxis')
+    body.appendChild(ycell)
 
 }
+
+const yAxisList = document.querySelectorAll('#yaxis.container')
+
+for (let i = 0; i < yaxis; i++) {
+    self = yAxisList[i]
+    for (let i = 0; i < xaxis; i++) {
+        let cell = document.createElement('div')
+        cell.classList.add('cell')
+        self.appendChild(cell)   
+    }
+
+}
+
+
