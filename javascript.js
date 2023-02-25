6ce54f70bb56c2e27cb67f3734a4590e0574a5a7
xBromsson <marvin@the-firebrand.com>
2023-02-25 02:06:16 -0600

    generated one line of the grid. x axis

--- javascript.js
+++ javascript.js
@@ -1,11 +1,13 @@
 const xaxis = 16
 const yaxis = 16
 
-/*
+const containerx = document.querySelector('#xaxis.container');
+
+
 for (let i = 0; i < xaxis; i++) {
 
     let cell = document.createElement('div')
-    cell.classList('cell')
+    cell.classList.add('cell')
+    containerx.appendChild(cell)
 
 }
-*/
