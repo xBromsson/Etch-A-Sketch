2c9e7bf9026eee2a6e87e6cfc263980749e62335
xBromsson <marvin@the-firebrand.com>
2023-02-26 13:19:51 -0600

    user can change grid size

--- javascript.js
+++ javascript.js
@@ -1,40 +1,73 @@
-const xaxis = 36
-const yaxis = 36
+
+//variables
 let isDragging = false;
-
 const mainContainer = document.querySelector('.main-container');
+let gridInput = document.querySelector(".grid")
+let xaxis = gridInput.value
+let yaxis = gridInput.value
 
-for (let i = 0; i < yaxis; i++) {
-    let ycell = document.createElement('div');
-    ycell.classList.add('container');
-    ycell.setAttribute('id','yaxis')
-    mainContainer.appendChild(ycell)
+//listeners
+gridInput.addEventListener("input", (event) => {
 
-}
+    if (gridInput.value > 100) {
+        gridInput.value = 100
+    }
 
-const yAxisList = document.querySelectorAll('#yaxis.container')
+    xaxis = gridInput.value;
+    yaxis = gridInput.value;
+    generateGrid();
 
-for (let i = 0; i < yaxis; i++) {
-    self = yAxisList[i]
-    for (let i = 0; i < xaxis; i++) {
-        let cell = document.createElement('div')
-        cell.classList.add('cell')
-        self.appendChild(cell) 
-        cell.addEventListener('mouseover', () => cell.classList.add('hover'))
-        cell.addEventListener('mouseout', () => cell.classList.remove('hover'))
-        cell.addEventListener('mousedown', (event) => {
-            event.preventDefault();
-            isDragging = true;
+})
+
+//functions
+function generateGrid(){
+
+    let currentGrid = document.querySelectorAll('#yaxis.container')
+
+    if(currentGrid instanceof NodeList) {
+        currentGrid.forEach(element => {
+            element.remove();
         });
-        cell.addEventListener('mouseup', () => isDragging = false)
-        cell.addEventListener('mousemove', (event) => {
-            if (isDragging){
-                cell.classList.add('draw') 
-            }
-        });
+    }
 
+    for (let i = 0; i < yaxis; i++) {
+        let ycell = document.createElement('div');
+        ycell.classList.add('container');
+        ycell.setAttribute('id','yaxis')
+        mainContainer.appendChild(ycell)
+    
+    }
+    
+    const yAxisList = document.querySelectorAll('#yaxis.container')
+    
+    for (let i = 0; i < yaxis; i++) {
+        self = yAxisList[i]
+        for (let i = 0; i < xaxis; i++) {
+            let cell = document.createElement('div')
+            cell.classList.add('cell')
+            self.appendChild(cell) 
+            cell.addEventListener('mouseover', () => cell.classList.add('hover'))
+            cell.addEventListener('mouseout', () => cell.classList.remove('hover'))
+            cell.addEventListener('mousedown', (event) => {
+                event.preventDefault();
+                isDragging = true;
+            });
+            cell.addEventListener('mouseup', () => isDragging = false)
+            cell.addEventListener('mousemove', (event) => {
+                if (isDragging){
+                    cell.classList.add('draw') 
+                }
+            });
+    
+        }
+    
     }
 
 }
+
+generateGrid();
+
+
+
 
 
